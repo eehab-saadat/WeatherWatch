309c071334f45846089ab2b7dc6114752286f82d
fayyez <fayyez2056@gmail.com>
2024-03-17 14:30:10 +0500

    added backend design

---
+++ src/main/java/org/algoavengers/weatherwatch/Backend/CacheManager.java
@@ -0,0 +1,8 @@
+package org.algoavengers.weatherwatch.Backend;
+
+public interface CacheManager {
+    // void write
+    // HashMap read
+    // boolean SearchByCoords
+    // boolean SearchByCity_Country
+}
--- src/main/java/org/algoavengers/weatherwatch/Backend/WeatherWatchService.java
+++ src/main/java/org/algoavengers/weatherwatch/Backend/WeatherWatchService.java
@@ -1,6 +1,7 @@
 package org.algoavengers.weatherwatch.Backend;
 
 import org.algoavengers.weatherwatch.Backend.WeatherWatchServiceInterface;
+import org.algoavengers.weatherwatch.Backend.APIHub;
 public class WeatherWatchService implements WeatherWatchServiceInterface {
     // data members
 }
