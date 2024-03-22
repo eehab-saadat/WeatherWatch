2a5ee6a7f49ef629b97a822f58b357bb92de88ef
1Zain <zainiqbal.35201@gmail.com>
2024-03-22 23:47:50 +0500

    added login page

--- src/main/java/org/algoavengers/weatherwatch/ui/GraphicalUI.java
+++ src/main/java/org/algoavengers/weatherwatch/ui/GraphicalUI.java
@@ -1,8 +1,28 @@
 package org.algoavengers.weatherwatch.ui;
 
-public class GraphicalUI implements DisplayInterface {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class GraphicalUI extends Application implements DisplayInterface {
+    private String API_KEY;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/org/algoavengers/weatherwatch/views/hello-view.fxml")));
+        primaryStage.setTitle("Hello View");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
     @Override
     public void run(String API_KEY) {
+        this.API_KEY = API_KEY;
         System.out.println("Displaying Graphical User Interface");
+        launch();
     }
 }
