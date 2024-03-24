207b23c7d8aaca8efb5d114619bc52eacd1961c7
akcena14 <116361254+akcena14@users.noreply.github.com>
2024-03-24 21:46:55 +0500

    Update DBManager.java
    made save protect duplication on all tables

--- src/main/java/org/algoavengers/weatherwatch/storage/DBManager.java
+++ src/main/java/org/algoavengers/weatherwatch/storage/DBManager.java
@@ -41,155 +41,154 @@ public class DBManager implements CacheManagerInterface {
             rs = getLastIdStatement.executeQuery(getLastIdSql);
             rs.next();
             locationId = rs.getInt(1);
+
+
+            // Insert weather data
+            String insertWeatherSql = "INSERT INTO WeatherDetails (location_id, temp, feelsLike, tempMin, tempMax, pressure, humidity, windSpeed, visibility, main, description, icon, sunrise, sunset, dt) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            PreparedStatement insertWeatherStatement = conn.prepareStatement(insertWeatherSql);
+            insertWeatherStatement.setInt(1, locationId);
+            insertWeatherStatement.setFloat(2, weatherData.getTemp());
+            insertWeatherStatement.setFloat(3, weatherData.getFeelsLike());
+            insertWeatherStatement.setFloat(4, weatherData.getTempMin());
+            insertWeatherStatement.setFloat(5, weatherData.getTempMax());
+            insertWeatherStatement.setFloat(6, weatherData.getPressure());
+            insertWeatherStatement.setFloat(7, weatherData.getHumidity());
+            insertWeatherStatement.setFloat(8, weatherData.getWindSpeed());
+            insertWeatherStatement.setFloat(9, weatherData.getVisibility());
+            insertWeatherStatement.setString(10, weatherData.getMain());
+            insertWeatherStatement.setString(11, weatherData.getDescription());
+            insertWeatherStatement.setString(12, weatherData.getIcon());
+            insertWeatherStatement.setString(13, weatherData.getSunrise());
+            insertWeatherStatement.setString(14, weatherData.getSunset());
+            insertWeatherStatement.setString(15, weatherData.getDt());
+            insertWeatherStatement.executeUpdate();
+
+            // Insert air pollution data
+            String insertAPSql = "INSERT INTO APDetails (location_id, aqi, co, no, no2, o3, so2, pm2_5, pm10, nh3, comment, dt) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            PreparedStatement insertAPStatement = conn.prepareStatement(insertAPSql);
+            insertAPStatement.setInt(1, locationId);
+            insertAPStatement.setInt(2, apData.getAqi());
+            insertAPStatement.setFloat(3, apData.getCo());
+            insertAPStatement.setFloat(4, apData.getNo());
+            insertAPStatement.setFloat(5, apData.getNo2());
+            insertAPStatement.setFloat(6, apData.getO3());
         }
-
-        // Insert weather data
-        String insertWeatherSql = "INSERT INTO WeatherDetails (location_id, temp, feelsLike, tempMin, tempMax, pressure, humidity, windSpeed, visibility, main, description, icon, sunrise, sunset, dt) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-        PreparedStatement insertWeatherStatement = conn.prepareStatement(insertWeatherSql);
-        insertWeatherStatement.setInt(1, locationId);
-        insertWeatherStatement.setFloat(2, weatherData.getTemp());
-        insertWeatherStatement.setFloat(3, weatherData.getFeelsLike());
-        insertWeatherStatement.setFloat(4, weatherData.getTempMin());
-        insertWeatherStatement.setFloat(5, weatherData.getTempMax());
-        insertWeatherStatement.setFloat(6, weatherData.getPressure());
-        insertWeatherStatement.setFloat(7, weatherData.getHumidity());
-        insertWeatherStatement.setFloat(8, weatherData.getWindSpeed());
-        insertWeatherStatement.setFloat(9, weatherData.getVisibility());
-        insertWeatherStatement.setString(10, weatherData.getMain());
-        insertWeatherStatement.setString(11, weatherData.getDescription());
-        insertWeatherStatement.setString(12, weatherData.getIcon());
-        insertWeatherStatement.setString(13, weatherData.getSunrise());
-        insertWeatherStatement.setString(14, weatherData.getSunset());
-        insertWeatherStatement.setString(15, weatherData.getDt());
-        insertWeatherStatement.executeUpdate();
-
-        // Insert air pollution data
-        String insertAPSql = "INSERT INTO APDetails (location_id, aqi, co, no, no2, o3, so2, pm2_5, pm10, nh3, comment, dt) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-        PreparedStatement insertAPStatement = conn.prepareStatement(insertAPSql);
-        insertAPStatement.setInt(1, locationId);
-        insertAPStatement.setInt(2, apData.getAqi());
-        insertAPStatement.setFloat(3, apData.getCo());
-        insertAPStatement.setFloat(4, apData.getNo());
-        insertAPStatement.setFloat(5, apData.getNo2());
-        insertAPStatement.setFloat(6, apData.getO3());
     }
 
     //find city from database
     public Object[] find(String city) throws SQLException {
-    Connection conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+        Connection conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
 
-    String sql = "SELECT l.*, wd.*, ap.* " +
-                 "FROM Locations l " +
-                 "INNER JOIN WeatherDetails wd ON l.id = wd.location_id " +
-                 "INNER JOIN APDetails ap ON l.id = ap.location_id " +
-                 "WHERE LOWER(l.name) = ?";  // Case-insensitive search
+        String sql = "SELECT l.*, wd.*, ap.* " +
+                "FROM Locations l " +
+                "INNER JOIN WeatherDetails wd ON l.id = wd.location_id " +
+                "INNER JOIN APDetails ap ON l.id = ap.location_id " +
+                "WHERE LOWER(l.name) = ?";  // Case-insensitive search
 
-    PreparedStatement statement = conn.prepareStatement(sql);
-    statement.setString(1, city.toLowerCase()); // Convert city to lowercase for comparison
-    ResultSet rs = statement.executeQuery();
+        PreparedStatement statement = conn.prepareStatement(sql);
+        statement.setString(1, city.toLowerCase()); // Convert city to lowercase for comparison
+        ResultSet rs = statement.executeQuery();
 
-    if (rs.next()) {
-        LocationData locationData = new LocationData();
-        locationData.id = rs.getInt("id");
-        locationData.name = rs.getString("name");
-        locationData.latitude = rs.getFloat("latitude");
-        locationData.longitude = rs.getFloat("longitude");
-        locationData.country = rs.getString("country");
+        if (rs.next()) {
+            LocationData locationData = new LocationData();
+            locationData.id = rs.getInt("id");
+            locationData.name = rs.getString("name");
+            locationData.latitude = rs.getFloat("latitude");
+            locationData.longitude = rs.getFloat("longitude");
+            locationData.country = rs.getString("country");
 
-        WeatherData weatherData = new WeatherData();
-        weatherData.id = rs.getInt("id"); // Optional (might not be used in your model)
-        weatherData.locationId = rs.getInt("location_id");
-        weatherData.temp = rs.getFloat("temp");
-        weatherData.feelsLike = rs.getFloat("feelsLike");
-        weatherData.tempMin = rs.getFloat("tempMin");
-        weatherData.tempMax = rs.getFloat("tempMax");
-        weatherData.pressure = rs.getFloat("pressure");
-        weatherData.humidity = rs.getFloat("humidity");
-        weatherData.windSpeed = rs.getFloat("windSpeed");
-        weatherData.visibility = rs.getFloat("visibility");
-        weatherData.main = rs.getString("main");
-        weatherData.description = rs.getString("description");
-        weatherData.icon = rs.getString("icon");
-        weatherData.sunrise = rs.getString("sunrise");
-        weatherData.sunset = rs.getString("sunset");
-        weatherData.dt = rs.getString("dt");
+            WeatherData weatherData = new WeatherData();
+            weatherData.id = rs.getInt("id"); // Optional (might not be used in your model)
+            weatherData.locationId = rs.getInt("location_id");
+            weatherData.temp = rs.getFloat("temp");
+            weatherData.feelsLike = rs.getFloat("feelsLike");
+            weatherData.tempMin = rs.getFloat("tempMin");
+            weatherData.tempMax = rs.getFloat("tempMax");
+            weatherData.pressure = rs.getFloat("pressure");
+            weatherData.humidity = rs.getFloat("humidity");
+            weatherData.windSpeed = rs.getFloat("windSpeed");
+            weatherData.visibility = rs.getFloat("visibility");
+            weatherData.main = rs.getString("main");
+            weatherData.description = rs.getString("description");
+            weatherData.icon = rs.getString("icon");
+            weatherData.sunrise = rs.getString("sunrise");
+            weatherData.sunset = rs.getString("sunset");
+            weatherData.dt = rs.getString("dt");
 
-        APData apData = new APData();
-        apData.id = rs.getInt("id"); // Optional (might not be used in your model)
-        apData.locationId = rs.getInt("location_id");
-        apData.aqi = rs.getInt("aqi");
-        apData.co = rs.getFloat("co");
-        apData.no = rs.getFloat("no");
-        apData.no2 = rs.getFloat("no2");
-        apData.o3 = rs.getFloat("o3");
-        apData.so2 = rs.getFloat("so2");
-        apData.pm2_5 = rs.getFloat("pm2_5");
-        apData.pm10 = rs.getFloat("pm10");
-        apData.nh3 = rs.getFloat("nh3");
-        apData.comment = rs.getString("comment");
-        apData.dt = rs.getString("dt");
+            APData apData = new APData();
+            apData.id = rs.getInt("id"); // Optional (might not be used in your model)
+            apData.locationId = rs.getInt("location_id");
+            apData.aqi = rs.getInt("aqi");
+            apData.co = rs.getFloat("co");
+            apData.no = rs.getFloat("no");
+            apData.no2 = rs.getFloat("no2");
+            apData.o3 = rs.getFloat("o3");
+            apData.so2 = rs.getFloat("so2");
+            apData.pm2_5 = rs.getFloat("pm2_5");
+            apData.pm10 = rs.getFloat("pm10");
+            apData.nh3 = rs.getFloat("nh3");
+            apData.comment = rs.getString("comment");
+            apData.dt = rs.getString("dt");
 
-        return new Object[]{locationData, weatherData, apData};
-    } else {
-        return null;
+            return new Object[]{locationData, weatherData, apData};
+        } else {
+            return null;
+        }
     }
-}
 
     //find top 5 city data
     public LocationData[] getTop5Locations() throws SQLException {
-    Connection conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+        Connection conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
 
-    String sql = "SELECT l.* " +
-                 "FROM Locations l " +
-                 "INNER JOIN WeatherDetails wd ON l.id = wd.location_id " +
-                 "ORDER BY wd.dt DESC " +  // Order by latest weather data timestamp (dt)
-                 "LIMIT 5";
+        String sql = "SELECT l.* " +
+                "FROM Locations l " +
+                "INNER JOIN WeatherDetails wd ON l.id = wd.location_id " +
+                "ORDER BY wd.dt DESC " +  // Order by latest weather data timestamp (dt)
+                "LIMIT 5";
 
-    PreparedStatement statement = conn.prepareStatement(sql);
-    ResultSet rs = statement.executeQuery();
+        PreparedStatement statement = conn.prepareStatement(sql);
+        ResultSet rs = statement.executeQuery();
 
-    List<LocationData> locations = new ArrayList<>();
-    while (rs.next() && locations.size() < 5) {
-        LocationData locationData = new LocationData();
-        locationData.id = rs.getInt("id");
-        locationData.name = rs.getString("name");
-        locationData.latitude = rs.getFloat("latitude");
-        locationData.longitude = rs.getFloat("longitude");
-        locationData.country = rs.getString("country");
+        List<LocationData> locations = new ArrayList<>();
+        while (rs.next() && locations.size() < 5) {
+            LocationData locationData = new LocationData();
+            locationData.id = rs.getInt("id");
+            locationData.name = rs.getString("name");
+            locationData.latitude = rs.getFloat("latitude");
+            locationData.longitude = rs.getFloat("longitude");
+            locationData.country = rs.getString("country");
 
-        locations.add(locationData);
+            locations.add(locationData);
+        }
+
+        return locations.toArray
     }
-
-    return locations.toArray
-}
 
     //delete outdated data from database
     public void deleteOutdatedRecords() throws SQLException {
-    Connection conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+        Connection conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
 
-    String currentDate = getCurrentDate();  // Get current date in desired format
+        String currentDate = getCurrentDate();  // Get current date in desired format
 
-    String sql = "DELETE FROM WeatherDetails wd " +
-                 "WHERE wd.dt < ?";  // Delete entries with dt before current date
+        String sql = "DELETE FROM WeatherDetails wd " +
+                "WHERE wd.dt < ?";  // Delete entries with dt before current date
 
-    PreparedStatement statement = conn.prepareStatement(sql);
-    statement.setString(1, currentDate);
-    statement.executeUpdate();
+        PreparedStatement statement = conn.prepareStatement(sql);
+        statement.setString(1, currentDate);
+        statement.executeUpdate();
 
-    // Cascade deletion will automatically remove corresponding entries from APDetails
+        // Cascade deletion will automatically remove corresponding entries from APDetails
 
-    conn.close();
-}
+        conn.close();
+    }
 
     private String getCurrentDate() {
-    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"); // Adjust format 
-    Calendar cal = Calendar.getInstance();
-    cal.add(Calendar.DAY_OF_MONTH, -1);  // Subtract 1 day from current date
-    return sdf.format(cal.getTime());
-}
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"); // Adjust format 
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.DAY_OF_MONTH, -1);  // Subtract 1 day from current date
+        return sdf.format(cal.getTime());
+    }
 
 
 
 }
-
-
