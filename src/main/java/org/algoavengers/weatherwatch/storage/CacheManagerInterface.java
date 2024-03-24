37683c9bdf520f3a7aa871ae8f923a6c95e8be2d
akcena14 <116361254+akcena14@users.noreply.github.com>
2024-03-25 00:53:42 +0500

    Update CacheManagerInterface.java
    changed to complement db manager

--- src/main/java/org/algoavengers/weatherwatch/storage/CacheManagerInterface.java
+++ src/main/java/org/algoavengers/weatherwatch/storage/CacheManagerInterface.java
@@ -2,10 +2,12 @@ package org.algoavengers.weatherwatch.storage;
 
 import org.algoavengers.weatherwatch.models.*;
 
+import java.sql.SQLException;
+
 public interface CacheManagerInterface {
-    void save(LocationData location, WeatherData weatherData, APData apData);
+    public void save(LocationData location, WeatherData weatherData, APData apData) throws SQLException;
     void delete(String city);
-    Object[] find(String city);
-    LocationData[] top5Locations();
-    void deleteOutdatedRecords();
+    public Object[] find(String city) throws SQLException;
+    public LocationData[] getTop5Locations() throws SQLException;
+    public void deleteOutdatedRecords() throws SQLException;
 }
